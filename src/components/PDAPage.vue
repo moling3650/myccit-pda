879e13fd54e6dea7d8f75ab265cba95f0fd7d2ec
moling <365024424@qq.com>
2018-06-22 15:35:09 +0800

    添加PDAPage页面

---
+++ src/components/PDAPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="PDAPage">PDAPage</div>
+</template>
+
+<script>
+export default {
+  name: 'PDAPage',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
